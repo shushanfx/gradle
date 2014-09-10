197ad79a66fcbf61ca37c30663fbba48e622a098
shushanfx <shushanfx@gmail.com>
2014-09-10 19:25:34 +0800

    Gradle第一次提交

---
+++ src/main/java/com/shushanfx/gradle/Main.java
@@ -0,0 +1,10 @@
+package com.shushanfx.gradle;
+
+/**
+ * Created by dengjianxin on 2014/9/11.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle...");
+    }
+}
